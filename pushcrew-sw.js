33bdc7374f45e742d348e09e8911e52c3f5bd3de
ratster <nikhil.khangar@wingify.com>
2017-10-04 17:59:15 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/263ca3009ebb198d0484e9248fa8addd.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/263ca3009ebb198d0484e9248fa8addd.js');
